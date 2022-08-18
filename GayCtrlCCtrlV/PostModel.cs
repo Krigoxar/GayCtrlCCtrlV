b4244c327cc2028bd97fd5766878a4a3064545e1
Krigoxar <94596936+Krigoxar@users.noreply.github.com>
2022-08-18 10:24:28 +0300

    Разработка в процесе

--- GayCtrlCCtrlV/PostModel.cs
+++ GayCtrlCCtrlV/PostModel.cs
@@ -6,12 +6,28 @@ using System.Threading.Tasks;
 
 namespace GayCtrlCCtrlV
 {
-    internal class PostModel
+    internal class ResponceModel
+    {
+        public ResponceModel()
+        {
+            
+        }
+        public Responce response { get; set; }
+    }
+
+    public class Responce 
+    {
+        public int count { get; set; }
+        public Dictionary<string, object>[] items { get; set; }
+    }
+
+    public class PostModel
     {
         public PostModel()
         {
 
         }
-
+        public int Id { get; set; }
+        public string Text { get; set; }
     }
 }
--- GayCtrlCCtrlV/Program.cs
+++ GayCtrlCCtrlV/Program.cs
@@ -1,4 +1,5 @@
 ﻿using RestSharp;
+using System.Text.Json;
 
 
 namespace GayCtrlCCtrlV
@@ -7,7 +8,8 @@ namespace GayCtrlCCtrlV
     {
         static void Main(string[] args)
         {
-            var options = new RestClientOptions("https://api.vk.com/method/wall.get?domain=studsovet_fksis&count=10&access_token=e47b8755e47b8755e47b875515e76bda39ee47be47b87558686bf8df746f8865cb20be1&v=5.131")
+            int count = 10;
+            var options = new RestClientOptions("https://api.vk.com/method/wall.get?domain=studsovet_fksis&count="+count+"&access_token=e47b8755e47b8755e47b875515e76bda39ee47be47b87558686bf8df746f8865cb20be1&v=5.131")
             {
                 //FollowRedirects = false,
                 Timeout = -1
@@ -16,7 +18,16 @@ namespace GayCtrlCCtrlV
             var request = new RestRequest();
             request.AddHeader("Cookie", "remixaudio_show_alert_today=0; remixff=0; remixlang=0; remixlgck=51e5db992f1baa2967; remixstid=554277290_M0wQzeI3LdBz2U4PaCMVBwmGiCtky3inuZhEcmhgMr8; remixstlid=9085104395135144281_v2HofaDQHCZsFeP80wUtFWzWrCZFwXAftMQp3EhVYcz; remixua=-1%7C-1%7C-1%7C1578911645");
             RestResponse response = client.Execute(request);
-            Console.WriteLine(response.Content);
+            //Console.WriteLine(response.Content);
+            if(response.Content != null)
+            {
+                ResponceModel person = JsonSerializer.Deserialize<ResponceModel>(response.Content);
+                foreach(Dictionary<string, object> item in person.response.items)
+                {
+                    Console.WriteLine("Новый Элемент:\n");
+                    Console.WriteLine(item["text"]);
+                }
+            }
         }
     }
 }
