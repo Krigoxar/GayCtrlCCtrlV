cf4b9fe6ceb347048125001c0c3e01b9761378e0
Krigoxar <94596936+Krigoxar@users.noreply.github.com>
2022-08-18 09:46:19 +0300

    Добавьте файлы проекта.

---
+++ GayCtrlCCtrlV/PostModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GayCtrlCCtrlV
+{
+    internal class PostModel
+    {
+        public PostModel()
+        {
+
+        }
+
+    }
+}
